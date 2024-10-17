f4affeb2abed2ca909637a76337334f92194b1ad
Axel <qianwangfan@ultraman.local>
2024-10-17 00:15:01 -0700

    replace add button with icons

--- screens/InfoDisplayScreen.js
+++ screens/InfoDisplayScreen.js
@@ -1,8 +1,11 @@
 import {View, Text, StatusBar, StyleSheet, Button, SafeAreaView, TouchableOpacity} from "react-native";
 import {ItemsList} from "../components/ItemsList";
-import React, { useState, useEffect, createContext, useContext } from "react";
+import React, {useState, useEffect, createContext, useContext} from "react";
 import {ItemContext} from "../context/ItemContext";
 import {ThemeContext, ThemeProvider} from '../context/Theme';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Ionicons from "@expo/vector-icons/Ionicons";
+import Feather from "@expo/vector-icons/Feather";
 
 /**
  * InfoDisplayScreen component displays a list of items filtered by type (activity or diet).
@@ -24,20 +27,35 @@ const InfoDisplayScreen = ({navigation, route}) => {
 
     useEffect(() => {
         navigation.setOptions({
-            headerRight: () => (
-                <View>
-                    <Button
-                        title="Add"
+            headerRight: () => {
+                let IconComponent;
+                let iconName;
+
+                if (route.name === 'Activities') {
+                    IconComponent = Feather;
+                    iconName = 'activity';
+                } else if (route.name === 'Diet') {
+                    IconComponent = Ionicons;
+                    iconName = 'fast-food-outline';
+                }
+                return (
+                    <TouchableOpacity
                         onPress={() => navigation.navigate('Entry', {type})}
-                    />
-                </View>
-            ),
+                        style={{marginRight: 15}}>
+                        <View style={{flexDirection: 'row'}}>
+                            <AntDesign name={'plus'} size={24} style={{marginRight: 2}}/>
+                            <IconComponent name={iconName} size={24}/>
+                        </View>
+                    </TouchableOpacity>
+
+                );
+            },
         });
-    }, [navigation, type]);
+    }, [navigation, route.name, type, theme]);
     // Filter items based on the current type (activity or diet)
     const filteredItemList = itemList.filter(item => item.type === type);
     return (<SafeAreaView>
-        <View >
+        <View>
             <ItemsList itemList={filteredItemList} type={type}/>
         </View>
     </SafeAreaView>);
